b87ab665c2848747f7a9fe5b57f827fadba6d313
tarokh <tarokh@Tarokhs-MacBook-Pro.local>
2018-12-07 18:59:03 +0330

    Added a new view contrroller

--- myGit1/ViewController.swift
+++ myGit1/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello Tarokh Mohammadi")
+        print("Tarokh Mohammadi is an iOS developer")
+        print("Hello baby")
     }
 
 
---
+++ myGit1/tarokhViewController.swift
@@ -0,0 +1,30 @@
+//
+//  tarokhViewController.swift
+//  myGit1
+//
+//  Created by Tarokh on 12/7/18.
+//  Copyright © 2018 Tarokh. All rights reserved.
+//
+
+import UIKit
+
+class tarokhViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("This is Tarokh viewController")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
